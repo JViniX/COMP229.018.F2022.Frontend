d5ca1e0ab2d6c5b6e6769ed15d60a06670ef9c38
JViniX <47263194+JViniX@users.noreply.github.com>
2022-11-17 14:41:05 -0500

    Created app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+
 import { AppComponent } from './app.component';
 
-import { IndexModule } from "./components/index.module";
-import { IndexComponent } from './components/index.component';
-
+import { IndexModule } from './components/index.module';
 import { InventoryModule } from "./components/inventory/inventory.module";
-import { AddEditComponent } from './components/inventory/add_edit.component';
-import { ListComponent } from './components/inventory/list.component';
-
-import { AuthGuard } from './components/auth/auth.guard';
 import { AuthModule } from './components/auth/auth.module';
-import { SignInComponent } from './components/auth/signin.component';
-import { SignUpComponent } from './components/auth/signup.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AuthGuard } from "./components/auth/auth.guard";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, 
-    FormsModule, 
+    BrowserModule,
     IndexModule,
     InventoryModule,
     AuthModule,
-    RouterModule.forRoot([
-      { path: "", component: IndexComponent},
-      { path: "inventory/list", component: ListComponent },
-      { path: "inventory/:mode", component: AddEditComponent, canActivate: [AuthGuard]},
-      { path: "inventory/:mode/:id", component: AddEditComponent, canActivate: [AuthGuard] },
-      { path: "users/signin", component: SignInComponent},
-      { path: "users/signup", component: SignUpComponent},
-      { path: "**", redirectTo: ""}
-    ])
+    AppRoutingModule
   ],
   providers: [
     AuthGuard
